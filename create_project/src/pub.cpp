f700b70c5a6abc7ee636b54f2420d55b6b40c6b7
Daniel Mushin <mushindv@gmail.com>
2023-07-23 19:47:25 +0500

    Write a simple publisher

--- create_project/src/pub.cpp
+++ create_project/src/pub.cpp
@@ -1,10 +1,42 @@
-#include <cstdio>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
 
-int main(int argc, char ** argv)
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
+class MinimalPublisher : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+  public:
+    MinimalPublisher()
+    : Node("publisher"), _count(0)
+    {
+      int n = 2;
+      using namespace std::chrono_literals;
+      _publisher = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      timer = this->create_wall_timer(
+      n * 1s, std::bind(&MinimalPublisher::timerCallback, this));
+    }
 
-  printf("hello world create_project package\n");
+  private:
+    void timerCallback()
+    {
+      auto message = std_msgs::msg::String();
+      message.data = "Hello, I'm alive!" + std::to_string(_count++);
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      _publisher->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _publisher;
+    size_t _count;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
   return 0;
 }
