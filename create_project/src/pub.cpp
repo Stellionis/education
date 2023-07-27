2fd8d4c07550d83ae1812888ce788caa8ca7ff42
Daniel Mushin <mushindv@gmail.com>
2023-07-27 16:38:53 +0500

    Fix seconds

--- create_project/src/pub.cpp
+++ create_project/src/pub.cpp
@@ -6,20 +6,18 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+static constexpr char* c_topic = "publisher";
+static const uint8_t c_queue_size = 10;
+static const uint8_t _time_interval_sec = 2;
 class MinimalPublisher : public rclcpp::Node
 {
   public:
-    static constexpr char* c_topic = "publisher";
-    static const uint8_t c_queue_size = 10;
-    const uint8_t _time_interval_sec = 2;
-
     MinimalPublisher()
     : Node("publisher"), _count(0)
     {
-      using namespace std::chrono;
       _ppublisher = this->create_publisher<std_msgs::msg::String>(c_topic, c_queue_size);
       _ptimer = this->create_wall_timer(
-      _time_interval_sec * 1s, std::bind(&MinimalPublisher::timerCallback, this));
+      std::chrono::seconds(_time_interval_sec), std::bind(&MinimalPublisher::timerCallback, this));
     }
 
   private:
