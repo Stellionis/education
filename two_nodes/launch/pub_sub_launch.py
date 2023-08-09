dfff3a4e14606b12cedb7956757bcfa72bea821f
Daniel Mushin <mushindv@gmail.com>
2023-08-09 20:17:49 +0500

    Remove cooments

--- two_nodes/launch/pub_sub_launch.py
+++ two_nodes/launch/pub_sub_launch.py
@@ -9,7 +9,6 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='two_nodes',
             executable='pub',
-            # name='pub',
             parameters=[os.path.join(get_package_share_directory
             ('two_nodes'), 'config', 'pub_params.yaml')],
             output="screen",
@@ -18,7 +17,6 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='two_nodes',
             executable='sub',
-            # name='sub',
             parameters=[os.path.join(get_package_share_directory
             ('two_nodes'), 'config', 'sub_params.yaml')],
             output="screen",
